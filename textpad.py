c35737a0e803fbc393744d1f2ad9bec943136173
I'jaaz Tello <ijaaztello@Ijaazs-MacBook-Pro.local>
2017-12-23 14:42:33 -0500

    bar chart using turtle

--- textpad.py
+++ textpad.py
@@ -1,8 +1,28 @@
-def print_square_root(x):
-    if x <= 0:
-        print("Positive numbers only, please.")
-        return
+import turtle
 
-    result = x**0.5
-    print("The square root of", x, "is", result)
-print_square_root(-4)
+def draw_bar(t, height):
+    """ Get turtle t to draw one bar, of height. """
+    t.begin_fill()
+    t.left(90)
+    t.forward(height) # Draw up the left side
+    t.right(90)
+    t.write('  ' + str(height)) 
+    t.forward(40) # Width of bar, along the top
+    t.right(90)
+    t.forward(height) # and down again!
+    t.left(90) # Put turtle facing the way we found it.
+    t.end_fill() 
+    t.penup()
+    t.forward(10) # leave smal gap after each bar
+    t.pendown()
+
+xs = [48, 117, 200, 240, 260, 220]
+
+wn = turtle.Screen()
+tess = turtle.Turtle()
+tess.color("blue", "red")
+tess.pensize(3)
+
+for v in xs:
+    draw_bar(tess, v)
+wn.mainloop()
