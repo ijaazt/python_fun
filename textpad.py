492690e6ef5a109a30df52f367379082685764cd
I'jaaz Tello <ijaaztello@Ijaazs-MacBook-Pro.local>
2017-12-24 13:04:44 -0500

    numbered days

--- textpad.py
+++ textpad.py
@@ -1,28 +1,13 @@
-import turtle
+day_num = int(input("What day Number? "))
+day_names = ["Sunday", "Monday", "Tuesday", "Wendnesday", "Thursday", "Fridaay", "Saturday"]
 
-def draw_bar(t, height):
-    """ Get turtle t to draw one bar, of height. """
-    t.begin_fill()
-    t.left(90)
-    t.forward(height) # Draw up the left side
-    t.right(90)
-    t.write('  ' + str(height)) 
-    t.forward(40) # Width of bar, along the top
-    t.right(90)
-    t.forward(height) # and down again!
-    t.left(90) # Put turtle facing the way we found it.
-    t.end_fill() 
-    t.penup()
-    t.forward(10) # leave smal gap after each bar
-    t.pendown()
+if day_num < 7 and day_num >= 0:
+    print(day_names[day_num])
+else:
+    day_num = int(input("There is only 7 days in a week. We start at 0: "))
+    if day_num > 6 or day_num < 0:
+       pass
+    else:
+        print(day_names[day_num])
 
-xs = [48, 117, 200, 240, 260, 220]
 
-wn = turtle.Screen()
-tess = turtle.Turtle()
-tess.color("blue", "red")
-tess.pensize(3)
-
-for v in xs:
-    draw_bar(tess, v)
-wn.mainloop()
